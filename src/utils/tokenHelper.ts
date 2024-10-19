9d6b5eabcf022311382b3299f84e9191a62140c5
yadul <yadulmanoj22@gmail.com>
2024-10-19 22:54:43 +0530

    Created Token Helper functions

--- src/utils/tokenHelper.ts
+++ src/utils/tokenHelper.ts
@@ -1,1 +1,7 @@
-export {};
+/*
+    Helper functions to get and remove token stored in local storage
+*/
+export const tokenHelper = {
+    getToken: () => localStorage.getItem("token"),
+    removeToken: () => localStorage.removeItem("token"),
+};
