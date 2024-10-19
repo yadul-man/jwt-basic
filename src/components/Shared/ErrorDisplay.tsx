5904a389152eb234ef4e8cffd50c2355bc26cff2
yadul <yadulmanoj22@gmail.com>
2024-10-20 00:10:08 +0530

    Initial implementation of Error Display component

--- src/components/Shared/ErrorDisplay.tsx
+++ src/components/Shared/ErrorDisplay.tsx
@@ -1,1 +1,11 @@
-export {};
+import React from "react";
+
+interface ErrorDisplayProps {
+    message: string;
+}
+
+const ErrorDisplay: React.FC<ErrorDisplayProps> = ({ message }) => {
+    return <p>{message}</p>;
+};
+
+export default ErrorDisplay;
