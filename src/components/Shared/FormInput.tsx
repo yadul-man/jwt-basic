77f41d3bae6b70690107df0adf0e409646a6ac29
yadul <yadulmanoj22@gmail.com>
2024-10-20 17:42:04 +0530

    Modified FormInput component

--- src/components/Shared/FormInput.tsx
+++ src/components/Shared/FormInput.tsx
@@ -1,1 +1,22 @@
-export {};
+interface FormInputProps {
+    label: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    type?: string;
+}
+
+const FormInput: React.FC<FormInputProps> = ({
+    label,
+    value,
+    onChange,
+    type = "text",
+}) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <input type={type} value={value} onChange={onChange} />
+        </div>
+    );
+};
+
+export default FormInput;
