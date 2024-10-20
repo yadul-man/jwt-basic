f99a4948ea4b4cf855112be7ed98ee446dc8204c
yadul <yadulmanoj22@gmail.com>
2024-10-20 15:42:59 +0530

    Created useAuth custom hook to specify the login, register and logout functionalities

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,1 +1,38 @@
-export {};
+import { useState } from "react";
+import AuthService from "../services/AuthService";
+import { AuthResponse } from "../models/AuthResponse";
+
+export const useAuth = () => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+        !!AuthService.getToken()
+    );
+
+    const login = async (email: string, password: string) => {
+        const result: AuthResponse | null = await AuthService.login(
+            email,
+            password
+        );
+        if (result && result.token) {
+            setIsAuthenticated(true);
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    const register = async (name: string, email: string, password: string) => {
+        const result: AuthResponse | null = await AuthService.register(
+            name,
+            email,
+            password
+        );
+        return result;
+    };
+
+    const logout = () => {
+        AuthService.logout();
+        setIsAuthenticated(false);
+    };
+
+    return { isAuthenticated, login, register, logout };
+};
