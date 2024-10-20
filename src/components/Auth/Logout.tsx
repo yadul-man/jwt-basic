eb3f92a0cc7a74d63008b9f192b590cee70e6363
yadul <yadulmanoj22@gmail.com>
2024-10-20 16:00:38 +0530

    Initial implementation of Logout component done

--- src/components/Auth/Logout.tsx
+++ src/components/Auth/Logout.tsx
@@ -1,1 +1,10 @@
-export {};
+import React from "react";
+import { useAuth } from "../../hooks/useAuth";
+
+const Logout: React.FC = () => {
+    const { logout } = useAuth();
+
+    return <button onClick={logout}>Logout</button>;
+};
+
+export default Logout;
