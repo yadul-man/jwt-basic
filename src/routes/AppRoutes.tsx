43e4e8a458939c7da37d3ced31936f253405d8bd
yadul <yadulmanoj22@gmail.com>
2024-10-20 18:40:56 +0530

    Modified App component and App Routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,1 +1,36 @@
-export {};
+import { Route, Routes, Navigate } from "react-router-dom";
+import Login from "../components/Auth/Login";
+import Register from "../components/Auth/Register";
+import { WeatherForecast } from "../components/WeatherForecast/WeatherForecast";
+import { useAuth } from "../hooks/useAuth";
+
+const ProtectedRoute = ({ element }: { element: JSX.Element }) => {
+    const { isAuthenticated } = useAuth();
+    return isAuthenticated ? element : <Navigate to="/login" />;
+};
+
+export const AppRoutes = () => {
+    const { isAuthenticated } = useAuth();
+    return (
+        <Routes>
+            <Route
+                path="/login"
+                element={
+                    !isAuthenticated ? <Login /> : <Navigate to="/weather" />
+                }
+            />
+            <Route
+                path="/register"
+                element={
+                    !isAuthenticated ? <Register /> : <Navigate to="/weather" />
+                }
+            />
+            <Route
+                path="/weather"
+                element={<ProtectedRoute element={<WeatherForecast />} />}
+            />
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+    );
+};
